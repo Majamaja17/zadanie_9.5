ef7323aae7875dedaf70261b8fa0f35716f3fd1a
Maja Jelenik <maja.jelenik@vp.pl>
2018-04-27 23:15:06 +0200

    Change js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
 window.onload = function() {
 
-	var withButtonItems = document.getElementsByClassName('button');
+	var withButtonItems = document.getElementsByClassName("button");
 	console.log(withButtonItems);
 
-	var ButtonItemsLength = withButtonItems.length;
-	console.log(ButtonItemsLength);
+	var buttonItemsLength = withButtonItems.length;
+	console.log(buttonItemsLength);
 
-	for (var j = 0; j < ButtonItemsLength; j++) {
+	for (var i = 0; i < buttonItemsLength; i++) {
 
-		var lastButton = document.getElementById('last_button');
-		var newParagraph = document.createElement('p');
+		var lastButton = document.getElementById("div");
+		var newParagraph = document.createElement("p");
 
-		newParagraph.innerText = withButtonItems;
+		newParagraph.innerText = withButtonItems[i].innerText;
 
 		lastButton.appendChild(newParagraph);
 		console.log(withButtonItems); 
 	}
-}
-
+};
